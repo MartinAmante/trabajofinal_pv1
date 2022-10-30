76379376831d3a2e6da4bead57a5952013a4dc6d
Ezemr12 <jelasesino09@gmail.com>
2022-10-30 01:03:19 -0300

    Mejora del mapa

---
+++ The cursed dungeon (copia)/Assets/Scripts/CamaraMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraMovement : MonoBehaviour
+{
+    private new Transform camera;
+    public Vector2 sensibility;
+
+    void Start()
+    {
+        camera = transform.Find("Main Camera");
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float Hor = Input.GetAxis("Mouse X");
+        float Ver = Input.GetAxis("Mouse Y");
+
+        if (Hor != 0)
+        {
+            transform.Rotate(Vector3.up * Hor * sensibility.x);
+        }
+        if (Ver != 0)
+        {
+            //camera.Rotate(Vector3.left * Ver * sensibility.y);
+            float angle = (camera.localEulerAngles.x - Ver * sensibility.y + 360) % 360;
+            if (angle > 180) { angle -= 360; }
+            angle = Mathf.Clamp(angle, -80, 80);
+            camera.localEulerAngles = Vector3.right * angle;
+        }
+    }
+}
---
+++ The cursed dungeon (copia)/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    Vector2 inputMov;
+    public float velCamina = 10f;
+    public float velCorrer = 20f;
+    public float rotationSpeed = 100;
+    private float x, y;
+    private bool HActivo; //se utiliza para saber si estan presionadas las teclas
+    private bool VActivo; //se utiliza para saber si estan presionadas las teclas
+    //public Animator anim;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        //anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputMov.x = Input.GetAxis("Horizontal");
+        inputMov.y = Input.GetAxis("Vertical");
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        float speed = Input.GetKey(KeyCode.LeftShift) ? velCorrer : velCamina;
+        rigidbody.velocity =
+            transform.forward * speed * inputMov.y //movernos hacia adelante y atras.
+            + transform.right * speed * inputMov.x; //movernos hacia izquierda y derecha.
+
+        //anim.SetFloat("VelX", x); //reproduce la animacion de correr y caminar.
+
+        //anim.SetFloat("VelY", y); //reproduce las animaciones de giro a los alterales.
+
+
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            if (VActivo == false)
+            {
+                HActivo = true;
+                //steep.Play();
+            }
+        }
+        if (Input.GetButtonDown("Vertical"))
+        {
+            if (HActivo == false)
+            {
+                VActivo = true;
+                //steep.Play();
+            }
+        }
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            HActivo = false;
+            if (VActivo == false)
+            {
+                //steep.Pause();
+            }
+        }
+        if (Input.GetButtonUp("Vertical"))
+        {
+            VActivo = false;
+            if (HActivo == false)
+            {
+                //steep.Pause();
+            }
+        }
+    }
+}
