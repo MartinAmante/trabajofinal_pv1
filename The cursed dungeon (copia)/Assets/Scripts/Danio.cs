176d1387f1e3f2feb429c0aaca6acae899704d83
farfandavid <70915094+farfandavid@users.noreply.github.com>
2022-11-05 21:00:52 -0300

    cambios

--- The cursed dungeon (copia)/Assets/Scripts/Danio.cs
+++ The cursed dungeon (copia)/Assets/Scripts/Danio.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DañoFlecha : MonoBehaviour {
+public class Danio : MonoBehaviour
+{
+    // Start is called before the first frame update
     private float danio;
     // Start is called before the first frame update
     void Start() {
